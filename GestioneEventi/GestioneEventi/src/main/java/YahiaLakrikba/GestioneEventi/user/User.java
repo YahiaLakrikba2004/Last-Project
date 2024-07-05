c008bda2cba1038d6d5f44f384238b60235d8912
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-07-05 11:33:50 +0200

    creating the application properties and initialized user class

---
+++ GestioneEventi/GestioneEventi/src/main/java/YahiaLakrikba/GestioneEventi/user/User.java
@@ -0,0 +1,80 @@
+package YahiaLakrikba.GestioneEventi.user;
+
+
+import jakarta.persistence.*;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    @Column(unique = true, nullable = true)
+    private String password;
+
+    private boolean isOrganizer;
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Event> events = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isOrganizer() {
+        return isOrganizer;
+    }
+
+    public void setOrganizer(boolean organizer) {
+        isOrganizer = organizer;
+    }
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+}
